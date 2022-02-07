f91b613a6e04a6a30c0d83bcdd6b710a60cfe60b
JadenFicklin <jadendficklin@gmail.com>
2022-02-07 10:19:55 -0700

    updates to signup page

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -16,29 +16,49 @@ function Signup() {
         <div className="signup-left-side">
           <div className="signup-left-side-border"></div>
         </div>
-        <div className="signup-right-side">
+        <form className="signup-right-side">
           <div className="signup-create-account-text">Create Account</div>
           <div className="signup-info-outer">
             <div className="signup-first-name-text">First name</div>
             <div className="signup-last-name-text">Last name</div>
             <div className="signup-first-name-last-name-outer">
-              <input className="signup-first-name-box" type="text"></input>
-              <input className="signup-last-name-box" type="text"></input>
+              <input
+                className="signup-first-name-box"
+                type="text"
+                placeholder="First name"
+              ></input>
+              <input
+                className="signup-last-name-box"
+                type="text"
+                placeholder="Last name"
+              ></input>
             </div>
             <div className="signup-email-text">Email</div>
-            <input className="signup-email-box" type="text"></input>
+            <input
+              className="signup-email-box"
+              type="email"
+              placeholder="Email"
+            ></input>
             <div className="signup-username-text">Username</div>
             <div className="signup-password-text">Password</div>
-            <input className="singup-username-box" type="text"></input>
-            <input className="singup-password-box" type="text"></input>
+            <input
+              className="singup-username-box"
+              type="text"
+              placeholder="Username"
+            ></input>
+            <input
+              className="singup-password-box"
+              type="password"
+              placeholder="Password"
+            ></input>
           </div>
           <div className="signup-bottom">
             <div className="signup-already-have-account">
               I already have an account
             </div>
-            <div className="signup-create-account">Create account</div>
+            <button className="signup-create-account">Create account</button>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
