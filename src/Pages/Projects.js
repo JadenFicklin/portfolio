3ce924b9f1ed83719e14370b74d6c1e7f711c607
JadenFicklin <jadendficklin@gmail.com>
2022-02-09 16:41:54 -0700

    added projects page styling

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -13,9 +13,19 @@ function Projects() {
     <>
       <Nav />
       <div className="projects-outer">
-        <div className="projects-left-outer"></div>
-        <div className="projects-right-outer"></div>
-        {/* <div className="projects-bottom-outer"></div> */}
+        <div className="projects-left-outer">
+          <div className="text-one-header">Project 1</div>
+          <div className="text-one">
+            Euka is an ecommerse website designed to sell shoes created as my
+            capstone project after the 8 week mark in DevMountain
+          </div>
+          <div className="text-two">Time frame: Jan 31 - Feb 10</div>
+          <div className="text-three">Tech stack: React</div>
+        </div>
+        <div className="projects-right-outer">
+          <div className="line"></div>
+          <div className="project-one"></div>
+        </div>
       </div>
     </>
   );
